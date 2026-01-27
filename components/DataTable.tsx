89a10ee0732ff23a4aba18da55de382a6bd477ad
Deba-07 <dynamicdeba07@gmail.com>
2026-01-27 20:21:59 +0530

    feat: introduce reusable DataTable with customizable columns and styles

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,31 +1,63 @@
 import { cn } from "@/lib/utils";
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
-import React from 'react'
+import React from "react";
 
-export const DataTable = <T, >({ columns, data, rowKey, tableClassName, headerClassName, headerRowClassName, headerCellClassName, bodyRowClassName, bodyCellClassName }: DataTableProps<T>) => {
+export const DataTable = <T,>({
+  columns,
+  data,
+  rowKey,
+  tableClassName,
+  headerClassName,
+  headerRowClassName,
+  headerCellClassName,
+  bodyRowClassName,
+  bodyCellClassName,
+}: DataTableProps<T>) => {
   return (
-    <Table className={cn('custom-scrollbar', tableClassName)}>
-        <TableHeader className={headerClassName}>
-            <TableRow className={cn('hover: bg-transparent!', headerRowClassName)}>
-                {columns.map((column, i) => (
-                    <TableHead key={i} className={cn('bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5')}>
-                        {column.header}
-                    </TableHead>
-                ))}
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-            {data.map((row, rowIndex) => (
-                <TableRow key={rowKey(row, rowIndex)} className={cn('overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative', bodyRowClassName)}>
-                    {columns.map((column, columnIndex) => (
-                        <TableCell key={columnIndex} className={cn('py-4 first:pl-5 last:pr-5')}>
-                            {column.cell(row, rowIndex)}
-                        </TableCell>
-                    ))}
-                </TableRow>
+    <Table className={cn("custom-scrollbar", tableClassName)}>
+      <TableHeader className={headerClassName}>
+        <TableRow className={cn("hover: bg-transparent!", headerRowClassName)}>
+          {columns.map((column, i) => (
+            <TableHead
+              key={i}
+              className={cn(
+                "bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5",
+              )}
+            >
+              {column.header}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row, rowIndex) => (
+          <TableRow
+            key={rowKey(row, rowIndex)}
+            className={cn(
+              "overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative",
+              bodyRowClassName,
+            )}
+          >
+            {columns.map((column, columnIndex) => (
+              <TableCell
+                key={columnIndex}
+                className={cn("py-4 first:pl-5 last:pr-5")}
+              >
+                {column.cell(row, rowIndex)}
+              </TableCell>
             ))}
-        </TableBody>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
-  )
-}
+  );
+};
