92605ac1afab0af199a75b3042dd9e683a8f0f81
Deba-07 <dynamicdeba07@gmail.com>
2026-01-28 20:13:38 +0530

    feat(home): create home page with CoinOverview and TrendingCoins using Suspense and fallbacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
+import { CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/Fallback";
 
-const page = () => {
+const page = async () => {
   return (
-    <div></div>
-  )
-}
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+        <CoinOverview />
+        </Suspense>
 
-export default page
+        <Suspense fallback={<TrendingCoinsFallback />}>
+        <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+};
+
+export default page;
