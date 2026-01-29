9f13b2b80e8ba36fe78c20713d1fa91010f04927
Deba-07 <dynamicdeba07@gmail.com>
2026-01-29 20:28:25 +0530

    feat(coin): add coin overview with OHLC candlestick chart

---
+++ components/home/CoinOverview.tsx
@@ -0,0 +1,45 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import { formatCurrency } from "@/lib/utils";
+import Image from "next/image";
+import { CoinOverviewFallback } from "./Fallback";
+import CandleStickChart from "../CandleStickChart";
+
+const CoinOverview = async () => {
+  try {
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("coins/bitcoin", {}),
+      fetcher<OHLCData[]>("coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        //interval: "hourly",
+        //precision: "full",
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <CandleStickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image
+              src={coin.image.large}
+              alt={coin.name}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+            </div>
+          </div>
+        </CandleStickChart>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching coin overview:", error);
+    return <CoinOverviewFallback />;
+  }
+};
+
+export default CoinOverview;
