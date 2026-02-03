4df550508c915c4e38b44bb01c245e1762f92b21
Deba-07 <dynamicdeba07@gmail.com>
2026-02-03 12:27:02 +0530

    feat: implement reusable CoinGecko API fetcher with error handling and revalidation

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -3,39 +3,37 @@
 import qs from "query-string";
 
 const BASE_URL = process.env.COINGECKO_BASE_URL;
-const API_KEY = process.env.COINGECKO_API_KEY;
 
-if (!BASE_URL) throw new Error("Could not get base url");
-if (!API_KEY) throw new Error("Could not get api key");
+if (!BASE_URL) throw new Error("Missing CoinGecko base url");
 
 export async function fetcher<T>(
   endpoint: string,
-  params?: QueryParams,
+  params?: Record<string, any>,
   revalidate = 60,
 ): Promise<T> {
+  const cleanEndpoint = endpoint.startsWith("/") ? endpoint : `/${endpoint}`;
+
   const url = qs.stringifyUrl(
     {
-      url: `${BASE_URL}/${endpoint}`,
+      url: `${BASE_URL}${cleanEndpoint}`,
       query: params,
     },
     { skipEmptyString: true, skipNull: true },
   );
 
+  // console.log("Fetching:", url);
+
   const response = await fetch(url, {
-    // headers: {
-    //   "x-cg-pro-api-key": API_KEY,
-    //   "Content-type": "application/json",
-    // } as Record<string, string>,
     next: { revalidate },
   });
 
   if (!response.ok) {
-    const errorBody: CoinGeckoErrorBody = await response
-      .json()
-      .catch(() => ({}));
+    const errorBody = await response.json().catch(() => ({}));
 
     throw new Error(
-      `API Error: ${response.status}: ${errorBody.error || response.statusText}`,
+      `CoinGecko API Error: ${response.status}: ${
+        errorBody.error || response.statusText
+      }`,
     );
   }
 
